825076c00388871f48a764868b7abe34ea0499a0
mek4doom <francisco.sole.15@gmail.com>
2022-12-24 15:36:50 -0300

    Delete utils_gui_c.lua

--- SZLogin/utils_gui_c.lua
+++
@@ -1,6 +0,0 @@
-function centerWindow (center_window)
-    local screenW, screenH = dgsGetScreenSize()
-    local windowW, windowH = dgsGetSize(center_window, false)
-    local x, y = (screenW - windowW) /2,(screenH - windowH) /2
-    return dgsSetPosition(center_window, x, y, false)
-end
